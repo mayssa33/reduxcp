64d7b8a3859f3ba2d8bdee1d76ec0d566172dc5c
mayssa33 <mayssamallek3@gmail.com>
2021-12-27 14:30:49 +0100

    1st

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Listtask from './component/Listtask';
+
+function App() {
+  
+  return (
+    <div className="App">
+    <Listtask/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/Typeaction.js
@@ -0,0 +1,6 @@
+
+export const ADD = "ADD";
+export const DELETE = "DELETE";
+export const COMPLETE= "COMPLETE";
+export const EDIT= "EDIT";
+export const FILTER= "FILTER";
